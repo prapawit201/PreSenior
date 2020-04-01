73a60be1072c62edfb8bbbf4a59cfdadcddbe8f4
prapawit201 <piyapat43@gmail.com>
2020-04-02 00:20:06 +0700

    initial commit

--- Docker composs/client/src/config/base_axios.js
+++ Docker composs/client/src/config/base_axios.js
@@ -3,7 +3,7 @@ import baseaxios from "axios";
 const axios = baseaxios.create({
   baseURL:
     process.env.NODE_ENV == "production"
-      ? "http://localhost:5000/api"
+      ? "http://localhost:5000/api" //ถ้ารันไม่ไดเปลี่ยนเป็น backend / frontend
       : "http://localhost:4000/api"
 });
 
