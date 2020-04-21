680283650b15263c8329d586192b5502d8b4a837
prapawit201 <piyapat43@gmail.com>
2020-04-21 17:10:50 +0700

    initial commit

--- LoginRegister/routes/Users.js
+++ LoginRegister/routes/Users.js
@@ -7,7 +7,7 @@ const bcrypt = require("bcrypt");
 const User = require("../models/User");
 users.use(cors());
 
-// process.env.SECRET_KEY = "secret";
+process.env.SECRET_KEY = "secret";
 
 users.post("/register", (req, res) => {
   const today = new Date();
@@ -44,6 +44,29 @@ users.post("/register", (req, res) => {
     })
     .catch((err) => {
       res.send("error" + err);
+    });
+});
+
+users.post("/login", (res, req) => {
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  })
+    .then((user) => {
+      if (user) {
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+          let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
+            expiresIn: 1440,
+          });
+          res.send(token);
+        }
+      } else {
+        res.status(400).json({ error: " User does not exist" });
+      }
+    })
+    .catch((err) => {
+      res.status(400).json({ error: err });
     });
 });
 module.exports = users;
