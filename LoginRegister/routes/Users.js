d1e0e1c5f6197f5fd9ff87459c6f1e66bca6ba0e
prapawit201 <piyapat43@gmail.com>
2020-04-22 15:32:59 +0700

    [Create] show profile

--- LoginRegister/routes/Users.js
+++ LoginRegister/routes/Users.js
@@ -70,4 +70,27 @@ users.post("/login", (req, res) => {
       res.status(400).json({ error: err });
     });
 });
+
+users.get("/profile", (req, res) => {
+  var decoded = jwt.verify(
+    req.headers["authorization"],
+    process.env.SECRET_KEY
+  );
+
+  User.findOne({
+    where: {
+      id: decoded.id,
+    },
+  })
+    .then((user) => {
+      if (user) {
+        res.json(user);
+      } else {
+        res.send("User does not exist");
+      }
+    })
+    .catch((err) => {
+      res.send("error: " + err);
+    });
+});
 module.exports = users;
