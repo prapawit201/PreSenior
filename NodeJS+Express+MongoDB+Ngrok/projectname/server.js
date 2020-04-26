4824706ba3f0179f46177c5e9493585ba0abc7b5
prapawit201 <piyapat43@gmail.com>
2020-04-27 00:14:19 +0700

    [Edit] Check error

--- NodeJS+Express+MongoDB+Ngrok/projectname/server.js
+++ NodeJS+Express+MongoDB+Ngrok/projectname/server.js
@@ -11,48 +11,51 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // mongodb+srv://seniorobd2:seniorobd2@seniorproject-f0o8e.mongodb.net/test?retryWrites=true&w=majority
 // mongodb://127.0.0.1:27017
 
-MongoClient.connect("mongodb+srv://seniorobd2:seniorobd2@seniorproject-f0o8e.mongodb.net/test?retryWrites=true&w=majority", (error, client) => {
-  if (error) throw error;
-  var db = client.db("test");
+MongoClient.connect(
+  "mongodb+srv://seniorobd2:seniorobd2@seniorproject-f0o8e.mongodb.net/test?retryWrites=true&w=majority",
+  (error, client) => {
+    if (error) throw error;
+    var db = client.db("test");
 
-  app.get("/", (req, res) => {
-    res.send("Hello World");
-  });
+    app.get("/", (req, res) => {
+      res.send("Hello World");
+    });
 
-  app.post("/test", function (req, res, next) {
-    console.log(req.body);
+    app.post("/test", function (req, res, next) {
+      console.log(req.body);
 
-    db.collection("users").insertOne(
-      {
-        vehicleSpeed: req.body.vehicleSpeed,
-        vehicleRPM: req.body.vehicleRPM,
-        RPM: req.body.RPM
-        // ArduinoNo: req.body.ArduinoNo,
-        // Temperature: req.body.Temperature,
-        // Humidity: req.body.Humidity,
-      },
-      (err, result) => {
-        if (err) return res.status(500).send(err.toString());
-        console.log("ok");
-        res.sendStatus(200);
-      }
-    );
-  });
+      db.collection("data").insertOne(
+        {
+          vehicle_speed: req.body.vehicle_speed,
+          oil: req.body.oil,
+          time: req.body.time,
+          // ArduinoNo: req.body.ArduinoNo,
+          // Temperature: req.body.Temperature,
+          // Humidity: req.body.Humidity,
+        },
+        (err, result) => {
+          if (err) return res.status(500).send(err.toString());
+          console.log("ok");
+          res.sendStatus(200);
+        }
+      );
+    });
 
-  app.get("/data", function (req, res, next) {
-    console.log(req.body);
+    app.get("/data", function (req, res, next) {
+      console.log(req.body);
 
-    db.collection("users")
-      .find()
-      .toArray((err, result) => {
-        if (err) return res.status(500).send(err.toString());
+      db.collection("data")
+        .find()
+        .toArray((err, result) => {
+          if (err) return res.status(500).send(err.toString());
 
-        res.status(200).send(result);
-      });
-  });
-  //! : เป็นการเชื่อม port ว่าเราจะไป portไหน
-  const PORT = process.env.PORT || 5555;
-  app.listen(PORT, () => {
-    console.log("server started : " + PORT);
-  });
-});
+          res.status(200).send(result);
+        });
+    });
+    //! : เป็นการเชื่อม port ว่าเราจะไป portไหน
+    const PORT = process.env.PORT || 5555;
+    app.listen(PORT, () => {
+      console.log("server started : " + PORT);
+    });
+  }
+);
