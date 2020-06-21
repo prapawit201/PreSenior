c4a6e8a4f7b833fbe6cd7cf9d7bd7dff989ea7a6
prapawit201 <piyapat43@gmail.com>
2020-06-21 23:15:29 +0700

    [Create] CRUD Full MERN stack

---
+++ CRUDReactJsNodeJs/backend/routes/employeeRoute.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+//importing controllers
+const employeeController = require("../controllers/customerController");
+router.get("/test", employeeController.test);
+
+router.get("/waka", (req, res) => {
+  res.json({ status: "Employeed Saved" });
+});
+
+module.exports = router;
