51cb6afcd7e4370a4b79eba72a21b58242ddb914
zBabEs <piyapat43@gmail.com>
2020-08-19 22:51:56 +0700

    [Create] backend upload image

--- UploadImage/index.js
+++ UploadImage/index.js
@@ -1,11 +1,25 @@
 const express = require("express");
-const port = 5000;
+const multer = require("multer");
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello Carlytics");
+const url = require("url");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + ".png");
+  },
 });
-
-app.listen(port, () => {
-  console.log("Hello World");
+const upload = multer({ storage: storage });
+app.get("/", (req, res) => {
+  res.send("Hello Upload");
+});
+app.post("/upload", upload.single("file"), (req, res) => {
+  let path = url;
+  console.log(res);
+  console.log(req.file.filename);
+  res.send(req.file);
+});
+app.listen(8001, () => {
+  console.log("service running at PORT: 8001");
 });
